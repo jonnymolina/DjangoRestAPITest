94b716bd6d8fe2dc2cb0d5aaf05d85819bdaa58b
Jonathan Molina <nemolina@calpoly.edu>
2016-02-05 18:04:03 -0800

    tutorial 4 login auth working

--- longtutorial/snippets/serializers.py
+++ longtutorial/snippets/serializers.py
@@ -1,8 +1,26 @@
-from rest_framework import serializers
+from rest_framework import serializers, permissions
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 # instead using serializers.Serializer, use ModelSerializer instead to avoid copy/pasting code from the models class
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+
+# represents the users added with createsuperuser
+class UserSerializer(serializers.ModelSerializer):
+    # snippets not included by default when using ModelSerializer
+    # since snippets is a reverse relationship on the User model.
+    # Solution is to add an explicit field for it here
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- longtutorial/snippets/views.py
+++ longtutorial/snippets/views.py
@@ -1,6 +1,9 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
 
 # This method is to go one step furthen from the mixin clases from before.
 # REST framework provides a set of already mixed-in generic views
@@ -8,7 +11,22 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    # the create() method of the serializer will now be passed
+    # an additional 'owner' field, along with the validated data
+    # from the request
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+# used for read-only views for the user representations
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# used for read-only views for the user representations
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
